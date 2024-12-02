9b81761de14bf672280cebc2eb948283e7c881ed
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-12-02 12:46:41 +0300

    adds parent & child class

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,14 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+pp= Car(37, 99)
+print(pp.wheel_number)
+print(pp.wheel_size)   
+print(pp.fill_up_tank())
+print(pp.go())    
+
+print(Car.__bases__)
+
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,18 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+            return "vrrrrrrrooom!"
+        
+    def fill_up_tank(self):
+            return "filling up!"
+        
+McLaren = Vehicle(70, "37")
+print(f"Wheel size:{McLaren.wheel_size} \nWheel Number: {McLaren.wheel_number}")
+print(f"My Car sounds like: {McLaren.go()}")
+print(f"I'm at the gas station {McLaren.fill_up_tank()}")
+
+
+
